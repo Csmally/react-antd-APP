953395efab658b6db9bf879049a869130901d1b6
Csmally <1130180692@qq.com>
2020-03-29 02:41:54 +0800

    antd-react练习项目

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Button, InputNumber} from 'antd'
+
+class AntdStudy extends React.Component{
+  render(){
+    return(
+      <div>
+        <Button>点击</Button>
+        <InputNumber/>
+      </div>
+    )
+  }
+}
+
+export default AntdStudy
